d5f9da7044591bf56dc0ff88acd2f01c5457c7c0
bravabam <karkibam7240lec@gmail.com>
2022-12-18 19:14:53 +0545

    ptr/100 today class

--- HelloBAM/EntryPoint.cs
+++ HelloBAM/EntryPoint.cs
@@ -1,22 +1,45 @@
-class Entry
+// class Entry
 
+// {
+//     public static void Main()
+//     {
+//         Console.ReadLine();
+
+//         var number = 50; // if 50 is even or odd how to know.
+
+//         if (number % 2 == 0)
+//         {
+
+//             Console.WriteLine(number + " is even");
+//         }
+//         else
+//         {
+//             Console.WriteLine(number + " is odd");
+
+//         }
+//     }
+// }
+// find simple interest for principal amount, time and rate given by user.
+
+class classork
 {
     public static void Main()
     {
-        var number = 50; // if 89 is even or odd how to know.
 
-        if (number % 2 == 0)
-        {
+        Console.WriteLine("principal amount");
+        int amount = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter a time period");
+        int time_period = int.Parse(Console.ReadLine());
+        Console.WriteLine("interest rate");
+        int interestrate = int.Parse(Console.ReadLine());
+        int simple_interest = amount * time_period * interestrate / 100;
+        Console.WriteLine("The simple interest =" + simple_interest);
 
-            Console.WriteLine(number + " is even");
-        }
-        else
-        {
-            Console.WriteLine(number + " is odd");
+        Console.ReadLine();
 
-        }
     }
-}
 
+
+}
 
 
