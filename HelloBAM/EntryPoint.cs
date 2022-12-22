b0ec646cf7ed970366c57c3fe7da4394a9f5518a
bravabam <karkibam7240lec@gmail.com>
2022-12-22 19:32:11 +0545

    class objects

--- HelloBAM/EntryPoint.cs
+++ HelloBAM/EntryPoint.cs
@@ -1,34 +1,29 @@
-class Entry
-{
-    public static void Main()
-    {
 
-
-        {
-            int num;
-            Console.WriteLine("Enter your check number:");
-            num = Convert.ToInt32(Console.ReadLine());
-            if (num % 3 == 0 && num % 5 == 0)
-            {
-                Console.WriteLine(num + " is even");
-                Console.WriteLine(num + " is a multiple of 3 and 5");
-            }
-            else if (num % 5 == 0)
-            {
-                Console.WriteLine(num + " is multiple of 5");
-            }
-            else if (num % 3 == 0)
-            {
-                Console.WriteLine(num + " is multiple of 3");
-            }
-            else
-            {
-                Console.WriteLine(num + " is odd");
-                Console.WriteLine(num + " is not multiple of 3 nor 5");
-            }
-        }
-    }
-}
+//         {
+//             int num;
+//             Console.WriteLine("Enter your check number:");
+//             num = Convert.ToInt32(Console.ReadLine());
+//             if (num % 3 == 0 && num % 5 == 0)
+//             {
+//                 Console.WriteLine(num + " is even");
+//                 Console.WriteLine(num + " is a multiple of 3 and 5");
+//             }
+//             else if (num % 5 == 0)
+//             {
+//                 Console.WriteLine(num + " is multiple of 5");
+//             }
+//             else if (num % 3 == 0)
+//             {
+//                 Console.WriteLine(num + " is multiple of 3");
+//             }
+//             else
+//             {
+//                 Console.WriteLine(num + " is odd");
+//                 Console.WriteLine(num + " is not multiple of 3 nor 5");
+//             }
+//         }
+//     }
+// }
 
 
 
@@ -52,3 +47,77 @@ class Entry
 //         }
 //     }
 // }
+
+
+// calculate compound interest for principle a...
+
+
+
+// class EntryPoint
+// {
+//     public static void Main()
+//     {
+
+//         Console.Write("Principle:");
+//         var pText = Console.ReadLine();
+//         var p = double.Parse(pText);
+
+//         Console.Write("Time(Years):");
+//         var tText = Console.ReadLine();
+//         var t = double.Parse(tText);
+
+//         Console.Write("Rate(%):");
+//         var rText = Console.ReadLine();
+//         var r = double.Parse(rText);
+
+//         var ci = p * Math.Pow((1 + r / 100), t) - p;
+//         // String interpolation
+//         Console.WriteLine($"Compound Interest of {p} for {t} years at {r}% is {ci}");
+
+//         Console.ReadKey();
+//     }
+// }
+
+// class EntryPoint
+// {
+
+//     public static void Main()
+//     {
+//         Person person1 = new();
+//         person1.name = "Bam karki";
+//         person1.age = 23;
+//     }
+
+// }
+
+
+// define a class representing an animal with at least 6 fields and a method. Create 5 objects/ instance from this class in our entry point
+
+// class Animal
+// {
+//     bool isDomestic;
+//     float height;
+//     string color;
+//     Boolean iswild;
+//     float weight;
+
+// }
+
+class Animal
+{
+
+    Animal animal1 = new();
+    Animal animal2 = new();
+    Animal animal3 = new();
+    Animal animal4 = new();
+    Animal animal5 = new();
+
+    void AnimalDetails()
+
+    {
+
+    }
+
+}
+
+
